f7d34c5554d711f24e3a3f80161a2b3a2d0ad5f3
Rajzik <jan.silhan@roihunter.com>
2019-03-18 09:17:11 +0100

    Fixed key

--- demo/src/const/pages/index.ts
+++ demo/src/const/pages/index.ts
@@ -1,11 +1,13 @@
 import { lazy } from 'react';
+
 import { IPages } from '../../types/pages';
 
 const pages: IPages[] = [
 	{
 		Component: lazy(() => import('../../pages/Main')),
 		props: {
-			path: '/'
+			path: '/',
+			key: '/'
 		}
 	}
 ];
